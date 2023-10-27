03d1c580162c9134c5415550822a4749d3dbdf0c
guilhermezkk <133978813+guilhermezkk@users.noreply.github.com>
2023-10-27 10:37:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert("ola mundo");
+function tocasompom() { 
+    document.querySelector(‘#som_tecla_pom’).play();
+}
+
+document.querySelector(‘#som_tecla_pom’).play();
+document.querySelector(‘#som_tecla_pom’).play();
