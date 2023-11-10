8dbb89f9e0383470fc95666f99ee4530a082efda
guilhermezkk <133978813+guilhermezkk@users.noreply.github.com>
2023-11-10 10:37:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocasompom() { 
+function tocasompom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('#som_tecla_pom').play();
-document.querySelector('#som_tecla_pom').play();
+//document.querySelector('#som_tecla_pom').play(); aqui o código faz tal coisa.
+document.querySelector('.tecla_pom').onclick = tocasompom;
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
